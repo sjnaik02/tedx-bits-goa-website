255fb616a45d22153149a4a633911cf8b8047954
Shourya Naik <sjnaik02@gmail.com>
2023-02-23 11:11:33 +0530

    added about section

---
+++ src/components/About.svelte
@@ -0,0 +1,3 @@
+<section class="h-[100svh] w-screen bg-black p-4 font-mono text-white">
+	<p class="break-words text-6xl  uppercase">We are India's oldest institutional TEDx Conference</p>
+</section>
