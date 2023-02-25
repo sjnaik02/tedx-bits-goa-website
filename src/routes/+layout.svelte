243297b9115a271451c15b6e2ae83eb09dd6f4bb
Shourya Naik <sjnaik02@gmail.com>
2023-02-26 03:20:00 +0530

    added some meta details

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,11 @@
 	import '../app.css';
 </script>
 
+<!-- add  a head  -->
+<svelte:head>
+	<title>TEDxBITSGoa</title>
+	<meta name="description" content="The official website for TEDxBITSGoa" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+</svelte:head>
+
 <slot />
