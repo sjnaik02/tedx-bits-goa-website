0b00a9b04d21e84912ec1ec48cb8ce11d60d399d
Shourya Naik <sjnaik02@gmail.com>
2023-02-28 23:40:59 +0530

    🔧Created a waitlist frontend

---
+++ src/components/Register.svelte
@@ -0,0 +1,22 @@
+<div class="flex h-screen w-full bg-black font-sans text-white">
+	<div class="flex h-full flex-col justify-center lg:w-1/2">
+		<div class="px-12">
+			<h1 class="text-6xl">Get your pass!</h1>
+			<p class="mt-4 text-2xl font-light ">
+				We are currently out of passes, join the waitlist to get notified when more are available!
+			</p>
+			<input
+				type="text"
+				class="mt-4 inline-block h-12 w-full rounded-lg border-2 border-white bg-black p-4 focus:border-red-600 focus:outline-none "
+				placeholder="Your Email Goes Here"
+			/>
+			<button
+				class="mt-4 inline-block rounded-lg border border-white py-2 px-16 text-xl uppercase transition duration-300 ease-in-out
+				 hover:bg-white hover:text-black focus:bg-white focus:text-black focus:outline-none"
+			>
+				Submit
+			</button>
+		</div>
+	</div>
+	<div class="w-1/2 bg-gradient-to-br from-red-900 via-black to-red-600" />
+</div>
