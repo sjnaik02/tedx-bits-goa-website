9cc633547e85e9b15ad339c62c030bab7b342f1e
Shourya Naik <sjnaik02@gmail.com>
2023-02-28 23:40:08 +0530

    some changes 🔧

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,15 +1,21 @@
 <section class=" h-screen  w-screen bg-black p-4 font-sans text-white">
-	<div class="max-w-3xl">
+	<div class="max-w-3xl lg:pt-36 lg:pl-12">
 		<p class="mt-12 text-5xl font-bold uppercase tracking-tighter">
 			India's <span class="text-red-600">oldest</span> institutional
 			<span class="text-red-600">TEDx</span> Conference
 		</p>
-		<p class="mt-24 text-2xl font-light">
+		<p class="mt-12 text-xl font-light">
 			Welcome to TEDxBITSGoa - where ideas come to life. As an independently organized TEDx event,
 			we're passionate about spreading ideas worth sharing. With live speakers and recorded TED
 			talks, we strive to inspire, educate, and spark meaningful conversations that lead to positive
 			change. Join us on our journey to explore new ideas, challenge perspectives, and create a
 			better world for ourselves and future generations. Let's make a difference together.
 		</p>
+		<a
+			class="text-md mt-12 inline-block font-light underline underline-offset-4 transition-colors  hover:text-red-600"
+			href="/"
+		>
+			Learn More about TED and TEDx →
+		</a>
 	</div>
 </section>
