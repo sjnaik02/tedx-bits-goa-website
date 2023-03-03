c26dda6a39107bed96e26122c08bf44df72ef8f1
Shourya Naik <sjnaik02@gmail.com>
2023-03-03 13:20:12 +0530

    ✨ styling improvements

---
+++ src/components/Waitlist.svelte
@@ -0,0 +1,27 @@
+<div class="flex h-screen w-full bg-black font-body text-white ">
+	<div class="flex h-full flex-col justify-center lg:w-1/2">
+		<div class="px-12">
+			<h1
+				class="relative z-10 font-heading text-6xl font-semibold
+				before:absolute before:left-0 before:top-8 before:-z-10 before:h-6 before:w-1/2 before:bg-red-800 before:opacity-100 before:content-['']"
+			>
+				Get your pass!
+			</h1>
+			<p class="mt-4 font-body text-2xl ">
+				We are currently out of passes, join the waitlist to get notified when more are available!
+			</p>
+			<input
+				type="text"
+				class="mt-4 inline-block h-12 w-full rounded-lg border-2 border-white bg-black p-4 font-body placeholder:font-body focus:border-red-600 focus:outline-none "
+				placeholder="Your Email Goes Here"
+			/>
+			<button
+				class="mt-4 inline-block rounded-lg border border-white py-2 px-16 text-xl uppercase transition duration-300 ease-in-out
+				 hover:bg-white hover:text-black focus:bg-white focus:text-black focus:outline-none"
+			>
+				Submit
+			</button>
+		</div>
+	</div>
+	<div class="w-1/2 bg-gradient-to-br from-red-900 via-black to-red-600" />
+</div>
