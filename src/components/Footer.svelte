374108774f01e21af93ae110597a287ccd583aab
Shourya Naik <sjnaik02@gmail.com>
2023-03-17 12:24:20 +0530

    added a basic footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,6 @@
+<footer
+	class="font-bod flex h-24 w-full flex-col items-center justify-center text-center	text-white  "
+>
+	<p class="text-lg">This event is operated under a licence from TED</p>
+	<p class="text-l">Created with <span class="text-red-600">❤</span> by Shourya Naik</p>
+</footer>
