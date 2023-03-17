92245c2d832bad0784635e639485ab2bcf1f9317
Shourya Naik <sjnaik02@gmail.com>
2023-03-17 12:23:45 +0530

    a functional speakers section, needs images

--- src/components/Speakers.svelte
+++ src/components/Speakers.svelte
@@ -5,21 +5,32 @@
 	import gradient from '$lib/images/blurry-gradient-haikei.svg';
 </script>
 
-<div class=" min-h-[100svh] w-screen overflow-hidden p-4 font-body text-white  ">
-	<h2
-		class="relative z-10 inline-block font-heading text-5xl font-bold uppercase
-         after:absolute after:left-0 after:top-6 after:-z-10 after:h-6 after:w-full  after:bg-red-600 after:opacity-60 after:content-['']"
-	>
-		Speakers
-	</h2>
-	<p class="relative mt-12 text-2xl text-white">
-		TEDxBITSGoa's speakers are a diverse and inspiring group of individuals from various fields and
-		backgrounds, all with the common goal of inspiring our audience to make a positive impact on the
-		world around them. We're proud to have hosted some of the most thought-provoking and inspiring
-		speakers in India.
-	</p>
-
-	<SpeakerCard image={speaker1} />
-	<SpeakerCard image={speaker2} />
-	<SpeakerCard image={speaker1} />
+<div class="flex min-h-screen w-screen p-4 font-body text-white lg:px-12">
+	<div class="sticky top-0 flex h-screen w-1/3 flex-col justify-center pb-40 pr-8">
+		<h2 class="relative z-10 inline-block font-heading text-5xl font-bold uppercase">
+			Meet Our <span class="text-red-600">Speakers</span>
+		</h2>
+		<p class="mt-12 text-xl text-white">
+			TEDxBITSGoa's speakers are a diverse and inspiring group of individuals from various fields
+			and backgrounds, all with the common goal of inspiring our audience to make a positive impact
+			on the world around them. We're proud to have hosted some of the most thought-provoking and
+			inspiring speakers in India.
+		</p>
+	</div>
+	<div class="ml-4 flex-1">
+		<div class="grid grid-cols-3 gap-4">
+			<SpeakerCard image={speaker1} />
+			<SpeakerCard image={speaker2} />
+			<SpeakerCard image={speaker1} />
+			<SpeakerCard image={speaker2} />
+			<SpeakerCard image={speaker1} />
+			<SpeakerCard image={speaker2} />
+			<SpeakerCard image={speaker1} />
+			<SpeakerCard image={speaker2} />
+			<SpeakerCard image={speaker1} />
+			<SpeakerCard image={speaker2} />
+			<SpeakerCard image={speaker1} />
+			<SpeakerCard image={speaker2} />
+		</div>
+	</div>
 </div>
