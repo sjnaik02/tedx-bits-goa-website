8dc81f51959d66f1a2aaa308473e6f6791081609
Shourya Naik <sjnaik02@gmail.com>
2023-03-20 01:52:37 +0530

    added minor responsiveness

--- src/components/Speakers.svelte
+++ src/components/Speakers.svelte
@@ -2,11 +2,12 @@
 	import SpeakerCard from './SpeakerCard.svelte';
 	import speaker1 from '$lib/images/Speaker1.jpg';
 	import speaker2 from '$lib/images/Speaker2.jpg';
-	import gradient from '$lib/images/blurry-gradient-haikei.svg';
 </script>
 
-<div class="flex min-h-screen w-screen p-4 font-body text-white lg:px-12">
-	<div class="sticky top-0 flex h-screen w-1/3 flex-col justify-center pb-40 pr-8">
+<div class="flex min-h-screen w-screen flex-col p-4 font-body text-white md:flex-row lg:px-12">
+	<div
+		class="top-0 flex flex-col justify-center py-10  pr-8 md:sticky md:h-screen md:w-1/3 md:pb-40"
+	>
 		<h2 class="relative z-10 inline-block font-heading text-5xl font-bold uppercase">
 			Meet Our <span class="text-red-600">Speakers</span>
 		</h2>
@@ -17,8 +18,8 @@
 			inspiring speakers in India.
 		</p>
 	</div>
-	<div class="ml-4 flex-1">
-		<div class="grid grid-cols-3 gap-4">
+	<div class="flex-1 md:ml-4">
+		<div class="grid grid-cols-2 gap-4  lg:grid-cols-3">
 			<SpeakerCard image={speaker1} />
 			<SpeakerCard image={speaker2} />
 			<SpeakerCard image={speaker1} />
