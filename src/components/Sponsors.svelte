bf081374eb31a5ea8b6672ab323c0f1b594dd67e
Shourya Naik <sjnaik02@gmail.com>
2023-03-20 01:53:03 +0530

    minor changes

--- src/components/Sponsors.svelte
+++ src/components/Sponsors.svelte
@@ -1,4 +1,4 @@
-<section class="min-h-[100svh] w-full bg-black  pl-12 text-center font-body text-white">
+<section class="min-h-[100svh] w-full bg-black text-center font-body text-white">
 	<h1
 		class="relative z-10 inline-block pt-36 font-heading text-5xl font-bold uppercase
     after:absolute after:left-0 after:bottom-0 after:-z-10 after:h-6 after:w-full after:bg-red-600 after:opacity-70
